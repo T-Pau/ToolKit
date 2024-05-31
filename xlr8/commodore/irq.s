8491bd007b6d71f193e02f31972aeb2002279935
Dieter Baron <dillo@nih.at>
2024-05-31 11:24:37 +0200

    Update to version from Anykey with support for other Commodore machines.

--- xlr8/commodore/irq.s
+++ xlr8/commodore/irq.s
@@ -1,8 +1,8 @@
 ;  irq.s -- Handle multiple raster IRQs.
 ;  Copyright (C) Dieter Baron
 ;
-;  This file is part of Joyride, a controller test program for C64.
-;  The authors can be contacted at <joyride@tpau.group>.
+;  This file is part of Toolkit.
+;  The authors can be contacted at <toolkit@tpau.group>.
 ;
 ;  Redistribution and use in source and binary forms, with or without
 ;  modification, are permitted provided that the following conditions
@@ -25,81 +25,244 @@
 ;  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 ;  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-; .define IRQ_DEBUG
+; IRQ_DEBUG = 1
+
+.pre_if .defined(C64)
+END_OF_IRQ = $ea81
+.define USE_VICII
+
+.pre_else_if .defined(C128)
+END_OF_IRQ = $ff33
+.define USE_VICII
+
+.pre_else_if .defined(MEGA65)
+.section code
+END_OF_IRQ {
+    pla
+    tay
+    pla
+    tax
+    pla
+    rti
+}
+.define USE_VICII
+
+.pre_else_if .defined(PLUS4)
+END_OF_IRQ = $fcbe
+.define USE_TED
+
+.pre_else_if .defined(VIC20)
+VIC_VERTICAL_ORIGIN = VIC_CONTROL_1
+
+END_OF_IRQ = $eb18
+.define USE_VIC
+
+.pre_else
+; .error "Target not supported"
+.pre_end
+
+.pre_if .defined(USE_VICII)
+BORDERCOLOR = VIC_BORDER_COLOR
+HLINE_LOW = VIC_RASTER
+HLINE_HIGH = VIC_CONTROL_1
+HLINE_BIT8 = $80
+IRR = VIC_INTERRUPT_REQUEST
+IMR = VIC_INTERRUPT_MASK
+IRQ_RASTER = 1
+
+.pre_else_if .defined(USE_VIC)
+
+BORDERCOLOR = VIC_COLOR
+
+.pre_else_if .defined(USE_TED)
+BORDERCOLOR = TED_BORDER_COLOR
+HLINE_LOW = $FF0B
+HLINE_HIGH = $FF0A
+HLINE_BIT8 = 1
+IRR = $FF09
+IMR = $FF0A
+IRQ_RASTER = 2
+.pre_end
 
 .section zero_page
 
-table .reserve 1
+table .reserve 2
 
 .section reserved
 
-irq_index .reserve 1
+index .reserve 1
 
 table_length .reserve 1
 
+.pre_if .defined(USE_VIC)
+next_line .reserve 1
+
+timer_length .reserve 2
+
+.public is_ntsc .reserve 1
+
+lines_per_frame .reserve 1
+.pre_end
 
 .section code
 
 .public init_irq {
     sei
-    ; disable cia 1 interrupts
-    lda #$7f
-    sta CIA1_INTERRUPT
+    .if .defined(USE_VICII) {
+        ; disable cia 1 interrupts
+        lda #$7f
+        sta CIA1_INTERRUPT
+        ; enable rasterline irq
+        lda #1
+        sta VIC_INTERRUPT_MASK
+    }
+    .else_if .defined(USE_VIC) {
+        ; set up constants for PAL/NTSC
+        ldx #0
+        ldy #312/2
+        lda VIC_VERTICAL_ORIGIN
+        cmp #$19
+        bne :+
+        inx
+        ldy #261/2
+    :   stx is_ntsc
+        sty lines_per_frame
 
-    ; enable rasterline irq
-    ldx #1
-    stx VIC_INTERRUPT_MASK
+        lda #$7f
+        sta VIA1_INTERRUPT_MASK    ; disable all interrupts on VIA1
+        sta VIA2_INTERRUPT_MASK    ; disable all interrupts on VIA2
+        ; set timer 1 to free run
+        lda	#$40
+        sta	VIA2_CONTROL_1
+        ; enable timer 1 interrupts
+        lda #$c0
+        sta VIA2_INTERRUPT_MASK
+        ; set initial run time to more than 1 frame
+        lda #$00
+        sta VIA2_TIMER_1_LOAD
+        lda #87
+        sta VIA2_TIMER_1_LOAD + 1
+    }
+    .else_if .defined(USE_TED) {
+        ; enable rasterline irq
+        ldx #IRQ_RASTER
+        stx IMR
+    }
 
     lda #<irq_main
-    sta $0314
+    sta IRQ_VECTOR
     lda #>irq_main
-    sta $0315
+    sta IRQ_VECTOR + 1
     cli
     rts
 }
+
 
 .public set_irq_table {
     stx table
     sty table + 1
     sta table_length
     lda #0
-    sta irq_index
+    sta index
     jmp setup_next_irq
 }
 
+
 irq_main {
+    .if .defined(USE_VIC) {
+        ;dec BORDERCOLOR
+        ; synchronize with raster line
+        lda next_line
+    :   cmp VIC_RASTER
+        bne :-
+        ;inc BORDERCOLOR
+    }
     .if .defined(IRQ_DEBUG) {
-        inc VIC_BORDER_COLOR
+        inc BORDERCOLOR
+    }
+    .if .defined(C128) {
+        lda #$0e
+        sta MMU_CONFIGURATION
     }
 .private irq_jsr:
     jsr $0000
     jsr setup_next_irq
     ; acknowledge irq
-    lda #1
-    sta VIC_INTERRUPT_REQUEST
-    .if .defined(IRQ_DEBUG) {
-        dec VIC_BORDER_COLOR
+    .if .defined(USE_VIC) {
+        lda VIA2_TIMER_1
     }
-    jmp $ea81
+    .else {
+        lda #IRQ_RASTER
+        sta IRR
+    }
+    .if .defined(IRQ_DEBUG) {
+        dec BORDERCOLOR
+    }
+    jmp END_OF_IRQ
 }
 
-setup_next_irq {
-    ldy irq_index
 
-    ; activate next entry
-    lda (table),y
-    sta VIC_RASTER
-    iny
-    lda (table),y
-    beq high0
-    lda VIC_CONTROL_1
-    ora #$80
-    sta VIC_CONTROL_1
-    bne addr
-high0:
-    lda VIC_CONTROL_1
-    and #$7f
-    sta VIC_CONTROL_1
+setup_next_irq {
+    ldy index
+
+    .if .defined(USE_VIC) {
+        lda #0
+        sta timer_length + 1
+        lda (table),y
+        sta next_line
+        sec
+        sbc VIC_RASTER
+        bcs :+
+        adc lines_per_frame
+    :	sbc #2
+        sta timer_length
+        asl
+        rol timer_length + 1    ; 10
+        asl
+        rol timer_length + 1    ; 100
+        asl
+        rol timer_length + 1    ; 1000
+        asl
+        rol timer_length + 1    ; 10000
+        ldx is_ntsc
+        bne :+
+        adc timer_length
+        bcc :+
+        inc timer_length + 1
+    :	asl
+        rol timer_length + 1    ; 1000x0
+        ldx is_ntsc
+        bne :+
+        adc timer_length
+        bcc :+
+        inc timer_length + 1
+    :	asl
+        rol timer_length + 1    ; 1x000xx
+        adc timer_length
+        bcc :+
+        inc timer_length + 1
+    :	asl
+        rol timer_length + 1    ; 1x000xx10
+        sta VIA2_TIMER_1
+        lda timer_length + 1
+        sta VIA2_TIMER_1 + 1
+    }
+    .else {
+        ; activate next entry
+        lda (table),y
+        sta HLINE_LOW
+        iny
+        lda (table),y
+        beq high0
+        lda HLINE_HIGH
+        ora #HLINE_BIT8
+        sta HLINE_LOW
+        bne addr
+    high0:
+        lda HLINE_HIGH
+        and #HLINE_BIT8 ^ $ff
+        sta HLINE_HIGH
+    }
 addr:
     iny
     lda (table),y
@@ -112,6 +275,6 @@ addr:
     cpy table_length
     bne :+
     ldy #0
-:   sty irq_index
+:	sty index
     rts
 }
