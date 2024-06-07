6282eeb08d954d801dfd5957910a45060ee2d56b
Dieter Baron <dillo@nih.at>
2024-06-07 19:18:24 +0200

    Add script to create zip archive.

--- python-packages/AtomicOutput.py
+++ python-packages/AtomicOutput.py
@@ -19,13 +19,17 @@ class AtomicOutput:
             raise RuntimeError("can't change only-if-changed after opening file")
         self.only_if_changed = value
 
-    def get_file(self):
-        if self.file is None:
+    def get_filename(self):
+        if self.temp_name is None:
             if self.only_if_changed:
                 self.temp_name = self.filename + "XXX"  # TODO: make unique name with pid
-                self.file = open(self.temp_name, "w")
             else:
-                self.file = open(self.filename, "w")
+                self.temp_name = self.filename
+        return self.temp_name
+
+    def get_file(self):
+        if self.file is None:
+            self.file = open(self.get_filename(), "w")
         return self.file
 
     def abort(self, message):
@@ -36,12 +40,12 @@ class AtomicOutput:
     def close(self):
         if self.file is not None:
             self.file.close()
-            # if self.only_if_changed: # TODO: move temp_name to filename
+            self.file = None
+        # if self.temp_name is not None and self.only_if_changed: # TODO: move temp_name to filename
 
     def discard(self):
         if self.file is not None:
             self.file.close()
-            if self.only_if_changed:
-                os.remove(self.temp_name)
-            else:
-                os.remove(self.filename)
+            self.file = None
+        if self.temp_name:
+            os.remove(self.temp_name)
